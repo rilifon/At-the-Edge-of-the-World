3559ca7bd28af2eba25e43be4c39645b326a6fee
Ricardo Lira <pudimdefrutas@gmail.com>
2023-09-14 19:13:50 -0300

    Better intro + started localization on main menu

--- project/localization/LocalizationKeys.gd
+++ project/localization/LocalizationKeys.gd
@@ -5,8 +5,22 @@ class_name LocalizationKeys
 
 const KEY_NAME = "KEY_NAME"
 const NEW_GAME = "NEW_GAME"
+const INTRO_1 = "INTRO_1"
+const INTRO_2 = "INTRO_2"
+const INTRO_3 = "INTRO_3"
+const INTRO_4 = "INTRO_4"
+const INTRO_5 = "INTRO_5"
+const INTRO_6 = "INTRO_6"
+const INTRO_7 = "INTRO_7"
 
 const KEYS = [
  "KEY_NAME",
- "NEW_GAME"
+ "NEW_GAME",
+ "INTRO_1",
+ "INTRO_2",
+ "INTRO_3",
+ "INTRO_4",
+ "INTRO_5",
+ "INTRO_6",
+ "INTRO_7"
 ]
