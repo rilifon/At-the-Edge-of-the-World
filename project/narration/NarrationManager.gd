7efdd2ee47379ec46889157b28492ae20e6a70e6
Ricardo Lira <pudimdefrutas@gmail.com>
2023-10-10 19:13:14 -0300

    Polishing game + added effect on distorted beast voice

--- project/narration/NarrationManager.gd
+++ project/narration/NarrationManager.gd
@@ -1,17 +1,24 @@
-extends Control
+extends CanvasLayer
 
 const NARRATION_BUS = 3
-const MIN_TIMER_RANGE = 60
-const MAX_TIMER_RANGE = 180
+const MIN_TIMER_RANGE = 1
+const MAX_TIMER_RANGE = 1.1
+const SCRAMBLE_DELAY = 1.0
 const AHAB_DELAY = .75
 const YOG_CUTOFF = 2.5
+const YOG_DIST_CUTOFF = 1.5
 const NARRATIONS_PATH = "res://assets/audio/narration/dialogues/"
 const NarrationDB = preload("res://narration/NarrationDB.gd")
+const SCRAMBLE_CYPHER = [
+	"a̵̢͖̠͔̲̺̫̦̅́͊̅̈̈́̔̇͛̈́̆̄̚̕͝","b̷̨̛̛̼̝͎̼̝͉͍̥̓́̽̔̌͐̿̑͘͜͠","c̷̠̀̌","d̷̨̪͖̺͇̗̣͉̻́̍͠͝","e̸̜̣̫͎̬̞͑̅͗̌̔̊͛͠","f̴̲̝͊́̚","g̷͍͙̮̜̞̰̲͉̫̲̩̔͗̑̚","h̶͙͚̺̯͎̼͎̘̖̱͚͖̙̫͂͑̓","ì̶̢͖̘̗̘̮̭̗̭̐̈́̑̋̂̈́̈͌̍͛̔̔̚͝","j̴̰̅͌̇̑","k̷̹͍̺͙̪͊̀͂̊̔́","l̶̙̖̳̯̫̀̈́͌̑͌̋͛̐̈́̓͘͝", "m̶̧͙̺͉̱̄͒̀̔̃͗̉̓̌̂͑͂̚͘͝",
+	"n̶̰͐̒̾̆͋̓̓͝","ó̸̫͍̲̪̉̊͗̓̔̀͝","p̶̘̃̈͋͐̂̍͆̕","q̴̜͔̣̥͙̳̱̘̟̣͎̖̼̈́̓̾̅͆͐̐͋̾͋̔̓̑̒̊͜ͅ","r̶̡̦̰̦͂́̅̒̿͒̀̈́́̕̚͜","s̴̡͈̣͇̜̞̗͇͍̠̈́͜","t̴̯̥͌̎͆͆͂̀͑͛͒͗͒͘","u̸̡̥̝̫̰͖̙̰̗̻͒̈́̏̐̀̾̚͠͠","v̵̡̹̞̹̫͕͚͍͌̈̊͌̊̂̑̕","w̷͇͙̫̔̈́̄͐̅̈́́̔͑̀́̅͝͝͝","x̷̨̡̟̗̤̗̩͈̮̭̅ͅ","ỹ̴̛͉̞̑̍̽͜͝", "z̷̡̹̣͚̤̻̥̰̜͕̎̍̍̽̕͜",
+]
 
 onready var Player = $Player
 onready var Subtitle = $Subtitle
 
 var timer = 0.0
+var scramble_timer = 0.0
 var cur_stage = 0
 var seen_narrations
 var is_running
@@ -42,14 +49,12 @@ func set_data(data):
 
 
 func enable_effect():
-	pass
-#	AudioServer.set_bus_effect_enabled(NARRATION_BUS, 0, true)
+	AudioServer.set_bus_effect_enabled(NARRATION_BUS, 0, true)
 
 
 
 func disable_effect():
-	pass
-#	AudioServer.set_bus_effect_enabled(NARRATION_BUS, 0, false)
+	AudioServer.set_bus_effect_enabled(NARRATION_BUS, 0, false)
 
 
 func new_timer():
@@ -69,9 +74,9 @@ func trigger_narration():
 	for i in range(cur_stage, -1, -1):
 		if DB[i].size() > seen_narrations[i].size():
 			#Still unseen narrations left
-			var new_idx = randi()%DB[i].size()
+			var new_idx = 0#randi()%DB[i].size()
 			while seen_narrations[i].has(new_idx):
-				new_idx = randi()%DB[i].size()
+				new_idx += 1#randi()%DB[i].size()
 			#Found an unseen narration
 			seen_narrations[i].append(new_idx)
 			start_narration(DB[i][new_idx])
@@ -83,21 +88,27 @@ func trigger_narration():
 
 func start_narration(narration):
 	for dialogue in narration:
-		add_subtitle(tr(dialogue.text))
-		var path = NARRATIONS_PATH + dialogue.voice
+		disable_effect()
 		
-		var dur = Player.stream.get_length()
+		add_subtitle(tr(dialogue.text))
+		
+		var path = NARRATIONS_PATH + dialogue.voice
+		var dur = 0
 		if dialogue.cha == "ahab":
-			disable_effect()
 			dur = dur + AHAB_DELAY
 		elif dialogue.cha == "yog":
-			dur = dur - YOG_CUTOFF
-			enable_effect()
 			if not Global.remove_distortion:
+				enable_effect()
+				apply_scramble()
+				dur = dur - YOG_DIST_CUTOFF
 				path = path.replace(".wav", "_dist.wav")
+			else:
+				dur = dur - YOG_CUTOFF
 		
 		Player.stream = load(path)
+		dur += Player.stream.get_length()
 		Player.play()
+		
 		yield(get_tree().create_timer(dur), "timeout")
 		
 	remove_subtitle()
@@ -107,5 +118,17 @@ func start_narration(narration):
 func add_subtitle(text):
 	Subtitle.text = text
 
+
 func remove_subtitle():
 	Subtitle.text = ""
+
+
+func apply_scramble():
+	var text = ""
+	for i in range(Subtitle.text.length()):
+		var c = Subtitle.text[i]
+		if c != " " and c != "," and c != ".":
+			text += SCRAMBLE_CYPHER[randi()%SCRAMBLE_CYPHER.size()]
+		else:
+			text += c
+	Subtitle.text = text
--- project/settings/Settings.gd
+++ project/settings/Settings.gd
@@ -1,4 +1,4 @@
-extends Control
+extends CanvasLayer
 
 onready var SoundSettings = {
 	"master": $VB/MasterSound,
